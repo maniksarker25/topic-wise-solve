a55282ef864e9e105dbaf34d4d4433c973e718b3
manik <maniksarker652@gmail.com>
2025-03-25 19:05:03 +0600

    solve counting problem

--- week-1/problem-1.cpp
+++ week-1/problem-1.cpp
@@ -1,2 +1,17 @@
 // Countring => https://atcoder.jp/contests/abc209/tasks/abc209_a?lang=en
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin >> a>>b;
+    if(a > b){
+        cout << 0<<endl;
+        return 0;
+    }
+    int diff = abs(a -b);
+    cout << diff +1;
+    return 0;
+}
