d2dfa5547e0a7ff3db32d3ebd3a9b0d0a9a2729c
manik <maniksarker652@gmail.com>
2025-04-06 21:12:39 +0600

    solve handle changing problem

--- week-2-stl-1/A_Olympiad_Date.cpp
+++ week-2-stl-1/A_Olympiad_Date.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 int main()
 {
-    cout << 'nice'<<endl;   
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
     return 0;
 }
---
+++ week-2-stl-1/B_Misha_and_Changing_Handles.cpp
@@ -0,0 +1,36 @@
+// Misha and Changing Handles => https://codeforces.com/problemset/problem/501/B
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    map<string,string>ans,has;
+    for(int i = 0;i<n;i++){
+        string a,b;
+        cin >> a>>b;
+
+        if(has.find(a) != has.end()){
+            string s = has[a];
+            ans[s] = b;
+            has.erase(a);
+            has[b] = s;
+        }
+
+    else{
+        ans[a] = b;
+        has[b] = a;
+    }
+    }
+
+    cout << ans.size() << endl;
+    for(auto [x,y]: ans){
+        cout << x << ' ' << y  << endl;
+    }
+
+    return 0;
+}
