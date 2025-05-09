6b7311a80aa2deb2b0dda244f2a1f3aa84a42968
manik <maniksarker652@gmail.com>
2025-05-09 08:30:37 +0600

    solve near lucky problemm

---
+++ week-6/near_lucky.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+bool isLucky(int num)
+{
+    if (num == 0)
+        return false;
+    while (num > 0)
+    {
+        int digit = num % 10;
+        if (digit != 4 && digit != 7)
+        {
+            return false;
+        }
+        num /= 10;
+    }
+    return true;
+}
+
+int main()
+{
+    string n;
+    cin >> n;
+    int luckyCount = 0;
+    for (char c : n)
+    {
+        if (c == '4' || c == '7')
+        {
+            luckyCount++;
+        }
+    }
+
+    if (isLucky(luckyCount))
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
